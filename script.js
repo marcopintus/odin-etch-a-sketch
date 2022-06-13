72a94c4492488ffa10a015dd85957e60993031e3
marcopintus <marco.pintus93@gmail.com>
2022-06-13 12:17:29 +0200

    Added the tiles change color to black when the mouse hovers over them

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ title.classList.add("title");
 title.textContent = "Etch-a-Sketch";
 document.body.appendChild(title);
 
-let canvasWidth = 800;
+let canvasWidth = 650;
 let canvasHeight = canvasWidth;
 
 let canvas = document.createElement("div");
@@ -15,9 +15,17 @@ let numTilesSide = 16;
 let tileDimension = canvasWidth/numTilesSide;
 
 for (let i=0; i<numTilesSide**2;i++){
-    console.log(i)
+    
     let tile = document.createElement("div");
     tile.classList.add("tile");
     tile.setAttribute("style",`width: ${tileDimension}px; height: ${tileDimension}px;`)
     canvas.appendChild(tile);
 }
+
+// queries
+let tiles = document.querySelectorAll(".tile");
+tiles.forEach((tl) =>{
+    tl.addEventListener('mouseenter',()=>{
+        tl.classList.add("ligth-up");
+    })
+})
